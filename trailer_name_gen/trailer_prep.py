98764122db4e7a3298c123a7ad944b353a70d4a6
saashimi <saavedra.kevin@gmail.com>
2018-03-21 07:38:27 -0700

    Remove unused files.

--- trailer_name_gen/trailer_prep.py
+++
@@ -1,9 +0,0 @@
-"""
-trailer_prep.py - A helper script to clean a list of trailer names
-"""
-
-def clean():
-    with open('raw_names.txt', 'r') as src:
-        with open('cleaned_names.txt', 'w') as dest:
-            for line in src:
-                dest.write('\"{0}\",\n'.format(line.rstrip('\n')))
