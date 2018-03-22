4acbe7b4259b584e53a72eea1b0bfdfe237de20f
saashimi <saavedra.kevin@gmail.com>
2018-03-22 08:32:09 -0700

    Update docstrings and eliminate unnecessary try/excepts.

--- callsign_gen/callsign_gen.py
+++ callsign_gen/callsign_gen.py
@@ -5,27 +5,33 @@ import pprint
 
 
 def callsign_one_word(dict_in, lst_in):
-    try:
-        if lst_in[1] in dict_in:
-            dict_in[lst_in[1]].append(lst_in[0])
-        else:
-            dict_in[lst_in[1]] = [lst_in[0]]
-    except KeyError:
-        return dict_in, lst_in
-
-    return dict_in, lst_in
+    """
+    Adds one-word callsigns to aircraft dictionary
+    Args:
+        dict_in: an aircraft dictionary
+        lst_in: a temporary list of strings from the aircraft callsign input
+                file
+    returns: None
+    """
+    if lst_in[1] in dict_in:
+        dict_in[lst_in[1]].append(lst_in[0])
+    else:
+        dict_in[lst_in[1]] = [lst_in[0]]
 
 
 def callsign_two_words(dict_in, lst_in):
-    try:
-        if lst_in[2] in dict_in:
-            dict_in[lst_in[2]].append(lst_in[0])
-        else:
-            dict_in[lst_in[2]] = [''.join(lst_in[0] + ' ' + lst_in[1])]
-    except KeyError:
-        return dict_in, lst_in
-
-    return dict_in, lst_in
+    """
+    Adds two-word callsigns to aircraft dictionary
+    Args:
+        dict_in: an aircraft dictionary
+        lst_in: a temporary list of strings from the aircraft callsign input
+                file
+    returns: None
+    """
+    if lst_in[2] in dict_in:
+        dict_in[lst_in[2]].append(lst_in[0])
+    else:
+        dict_in[lst_in[2]] = [''.join(lst_in[0] + ' ' + lst_in[1])]
 
 
 def main():
@@ -37,11 +43,14 @@ def main():
             temp_list = line.split()
             try:
                 if '-' in temp_list[1]:
+                    temp_list[1] = temp_list[1].replace(',', '')
                     callsign_one_word(ac_dict, temp_list)
 
                 elif '-' in temp_list[2]:
+                    temp_list[2] = temp_list[2].replace(',', '')
                     callsign_two_words(ac_dict, temp_list)
             except IndexError:
+                print("There was an index error")
                 continue
 
     for ac in ac_dict:
